6b486f0f64af9cce1317292728c943d0363485ec
Sreekanth Mk <sreekanth.mku@gmail.com>
2025-01-07 19:59:17 +0530

    Update self to this to fix vercel deploy error

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,19 +1,18 @@
-
 const CACHE_NAME = 'dtc-cache-v1';
 const urlsToCache = ['/', '/index.html', '/static/js/bundle.js'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+this.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+this.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
