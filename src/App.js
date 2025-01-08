1a6a67c4874aa725842236734a9a15ff2da23dcf
Dilipkumar15737 <dilip.sabado@gmail.com>
2025-01-08 14:42:14 +0530

    responsive design

--- src/App.js
+++ src/App.js
@@ -1,56 +1,55 @@
-import React ,{ useState, useEffect }from 'react';
-import { Route, Routes } from 'react-router-dom';
-import DtcList from './components/DtcList';
-import PWADialog from "../src/components/PWADialog"; 
-
-
-
-
-
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import DtcList from "./components/DtcList";
+import Troubleshoot from "./components/Troubleshoot";
+import PWADialog from "./components/PWADialog";
 
 const App = () => {
-  const [isPWAInstallable, setIsPWAInstallable] = useState(false)
-const [deferredPrompt, setDeferredPrompt] = useState(null);
-const [isOpen, setIsOpen] = useState(false)
-const isPublicRoute = false;
-useEffect(() => {
-  const handleBeforeInstallPrompt = (event) => {
-    event.preventDefault()
-    setIsPWAInstallable(true)
-    setTimeout(() => setIsOpen(true), 2000)
-    setDeferredPrompt(event)
-  }
+  const [isPWAInstallable, setIsPWAInstallable] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setIsPWAInstallable(true);
+      setTimeout(() => setIsOpen(true), 2000);
+      setDeferredPrompt(event);
+    };
 
-  return () => {
-    window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
-  }
-}, [])
-const handleAddToHomeScreen = async () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt()
-    const { outcome } = await deferredPrompt.userChoice
-    setIsPWAInstallable(false)
-    setDeferredPrompt(null)
-    setIsOpen(false)
-  }
-}
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleAddToHomeScreen = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      setIsPWAInstallable(false);
+      setDeferredPrompt(null);
+      setIsOpen(false);
+    }
+  };
 
   return (
     <>
-     {isPWAInstallable &&   (
-        <PWADialog isOpen={isOpen} setIsOpen={setIsOpen} onInstall={handleAddToHomeScreen} />
+      {isPWAInstallable && (
+        <PWADialog
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          onInstall={handleAddToHomeScreen}
+        />
       )}
-    
-    <Routes>
-      <Route path="/" element={<DtcList />} />
-      <Route
-     
-        path="/troubleshoot/:id"
-        element={<div className="text-center mt-10">Troubleshoot Page</div>}
-      />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<DtcList />} />
+        <Route path="/troubleshoot/:id" element={<Troubleshoot />} />
+      </Routes>
     </>
   );
 };
--- src/components/DtcList.jsx
+++ src/components/DtcList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const dtcList = [
+const initialDtcList = [
   {
     id: "P2227",
     description: 'Barometric Pressure Sensor "A" Circuit Range/Performance',
@@ -9,7 +9,8 @@ const dtcList = [
   },
   {
     id: "P2138",
-    description: 'Throttle/Pedal Position Sensor/Switch "D"/"E" Voltage Correlation.',
+    description:
+      'Throttle/Pedal Position Sensor/Switch "D"/"E" Voltage Correlation.',
     status: "Inactive",
     troubleshootUrl: "https://pwadev.elisa.live/dtc/p2138",
   },
@@ -29,65 +30,63 @@ const dtcList = [
 
 const DtcList = () => {
   const handleTroubleshoot = (url) => {
-    window.location.href = url; 
+    window.location.href = url;
   };
 
   return (
-   <div className="h-screen flex justify-center items-center">
-     <div className="bg-grey-100 flex flex-col items-center px-4 h-[99vh]">
-      
-<div className="border border-[#000000] h-full flex-grow flex flex-col items-center">
-    
-      <div className="flex-grow w-full max-w-md "> 
-      
-      <div className="bg-blue-500 text-white w-full py-4 px-3 text-left text-xl font-bold shadow-md">
-        DTC List
-      </div>
-        {dtcList.map((item, index) => (
-          <div
-            key={index}
-            className="bg-white shadow -lg p-2  border relative"
-          >
-            
-            <div className="flex items-center justify-between mb-1">
-              <span className="text-lg font-semibold text-gray-800">
-                {item.id}
-              </span>
-              <span
-                className={`${
-                  item.status === "Active" ? "text-red-500" : "text-gray-500"
-                } font-bold`}
-              >
-                {item.status}
-              </span>
+    <div className="h-screen flex justify-center items-center ">
+      <div className="bg-grey-100 flex flex-col items-center   h-[100vh] w-full">
+        <div className="border border-[#000000] h-full flex-grow flex flex-col items-center w-full ">
+          <div className="flex-grow w-full">
+            <div className="bg-blue-500 text-white w-full py-4 px-3 text-left text-xl font-bold shadow-md">
+              DTC List
             </div>
+            {initialDtcList.map((item, index) => (
+              <div
+                key={index}
+                className="bg-white shadow-lg p-2 border relative mb-2 mx-2"
+              >
+                <div className="flex items-center justify-between mb-1">
+                  <span className="text-lg font-semibold text-gray-800">
+                    {item.id}
+                  </span>
+                  <span
+                    className={`${
+                      item.status === "Active"
+                        ? "text-red-500"
+                        : "text-gray-500"
+                    } font-bold`}
+                  >
+                    {item.status}
+                  </span>
+                </div>
 
-            
-            <div className="flex items-center justify-between">
-              <span className="text-sm text-gray-600">{item.description}</span>
-              <button
-                onClick={() => handleTroubleshoot(item.troubleshootUrl)}
-                className="bg-blue-500 text-white text-sm font-medium py-1 px-3 rounded hover:bg-blue-600"
-              >
-                Troubleshoot
-              </button>
-            </div>
+                <div className="flex items-center justify-between">
+                  <span className="text-sm text-gray-600">
+                    {item.description}
+                  </span>
+                  <button
+                    onClick={() => handleTroubleshoot(item.troubleshootUrl)}
+                    className="bg-blue-500 text-white text-sm font-medium py-1 px-3 rounded hover:bg-blue-600"
+                  >
+                    Troubleshoot
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
 
-      
-      <div className="w-full max-w-md flex justify-between mt-4 mb-4 px-4">
-        <button className="bg-blue-500 text-white py-2 px-20  shadow hover:bg-blue-600">
-          REFRESH
-        </button>
-        <button className="bg-red-500 text-white py-2 px-20  shadow hover:bg-red-600">
-          CLEAR
-        </button>
+          <div className="w-full flex justify-between mt-4 mb-4 px-4 gap-2">
+            <button className="bg-blue-500 text-white py-2 px-4 sm:px-20 shadow hover:bg-blue-600 flex-1">
+              REFRESH
+            </button>
+            <button className="bg-red-500 text-white py-2 px-4 sm:px-20 shadow hover:bg-red-600 flex-1">
+              CLEAR
+            </button>
+          </div>
+        </div>
       </div>
-</div>
     </div>
-   </div>
   );
 };
 
